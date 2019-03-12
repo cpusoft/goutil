4212a01ce2db343d008b4b72a99e8aa456ab5c2b
cpusoft <cpusoft@live.com>
2019-03-12 16:49:33 +0800

    add

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	DefaultUserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
-	DefaultTimeout   = 5
+	DefaultTimeout   = 30
 )
 
 func Get(urlStr string) (resp gorequest.Response, body string, err error) {
