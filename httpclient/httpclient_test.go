d252ac3b65c0b3d937ea807608588ce1b7de3f2e
cpusoft <cpusoft@live.com>
2019-03-13 17:15:25 +0800

    test TLS

--- httpclient/httpclient_test.go
+++ httpclient/httpclient_test.go
@@ -9,6 +9,18 @@ import (
 
 func TestGet(t *testing.T) {
 	//往rp发送请求
+	resp, body, err := httpclient.Get("http://localhost:8080/hello")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("resp: %+v", resp)
+	fmt.Println("body: %+v", body)
+}
+
+func TestGetTLS(t *testing.T) {
+	//往rp发送请求
 	resp, body, err := httpclient.GetTLS("https://localhost:8081/hello")
 
 	if err != nil {
