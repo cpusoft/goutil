2dde526c32a8b2641c48b43a8e056b20c5ae45d3
cpusoft <cpusoft@live.com>
2019-03-18 10:57:45 +0800

    add

---
+++ hashutil/hashutil.go
@@ -0,0 +1,27 @@
+package hashutil
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+)
+
+func Md5(data string) string {
+	md5 := md5.New()
+	md5.Write([]byte(data))
+	md5Data := md5.Sum([]byte(""))
+	return hex.EncodeToString(md5Data)
+}
+
+func Hmac(key, data string) string {
+	hmac := hmac.New(md5.New, []byte(key))
+	hmac.Write([]byte(data))
+	return hex.EncodeToString(hmac.Sum([]byte("")))
+}
+
+func Sha1(data string) string {
+	sha1 := sha1.New()
+	sha1.Write([]byte(data))
+	return hex.EncodeToString(sha1.Sum([]byte("")))
+}
