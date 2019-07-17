4c941aea325377cae0869671b0b56d03de0d8d30
cpusoft <cpusoft@live.com>
2019-07-17 11:48:41 +0800

    add

--- datetime/datetime.go
+++ datetime/datetime.go
@@ -13,3 +13,9 @@ func Now() string {
 func ToString(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
 }
+
+//const TIME_LAYOUT = "2006-01-02 15:04:05"
+//tm, e := time.Parse("060102150405Z", "190601095044Z")
+func ParseTime(s string, timeLayout string) (tm time.Time, err error) {
+	return time.Parse(timeLayout, s)
+}
---
+++ datetime/datetime_test.go
@@ -0,0 +1,12 @@
+package datetime
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestXYZ(t *testing.T) {
+	TIME_LAYOUT := "060102150405Z"
+	tm, e := ParseTime("190601095044Z", TIME_LAYOUT)
+	fmt.Println(tm, e)
+}
