267ceb35b7e23d94a46d918933e7d6b5a0611c37
cpusoft <cpusoft@live.com>
2019-08-03 21:56:35 +0800

    add

--- cert/cert.go
+++ cert/cert.go
@@ -8,8 +8,72 @@ import (
 	belogs "github.com/astaxie/beego/logs"
 )
 
+func VerifyCertByX509(fatherCertFile string, childCertFile string) (result string, err error) {
+	fatherFileByte, err := ioutil.ReadFile(fatherCertFile)
+	if err != nil {
+		belogs.Debug("VerifyCertsByX509():fatherCertFile:", fatherCertFile, "   ReadFile err:", err)
+		return "fail", err
+	}
+	childFileByte, err := ioutil.ReadFile(childCertFile)
+	if err != nil {
+		belogs.Debug("VerifyCertsByX509():childCertFile:", childCertFile, "   ReadFile err:", err)
+		return "fail", err
+	}
+	return VerifyCertByteByX509(fatherFileByte, childFileByte)
+}
+
+func VerifyEeCertByX509(fatherCertFile string, mftRoaFile string, eeCertStart, eeCertEnd uint64) (result string, err error) {
+	fatherFileByte, err := ioutil.ReadFile(fatherCertFile)
+	if err != nil {
+		belogs.Debug("VerifyCertsByX509():fatherCertFile:", fatherCertFile, "   ReadFile err:", err)
+		return "fail", err
+	}
+	mftRoaFileByte, err := ioutil.ReadFile(mftRoaFile)
+	if err != nil {
+		belogs.Debug("VerifyCertsByX509():childCertFile:", mftRoaFile, "   ReadFile err:", err)
+		return "fail", err
+	}
+	b := mftRoaFileByte[eeCertStart:eeCertEnd]
+	return VerifyCertByteByX509(fatherFileByte, b)
+}
+
 // fatherCerFile is as root, childCerFile is to be verified
 // result: ok/fail
+func VerifyCertByteByX509(fatherCertByte []byte, childCertByte []byte) (result string, err error) {
+	belogs.Debug("VerifyCertBytesByX509():fatherCertByte:", len(fatherCertByte), "   childCertByte:", len(childCertByte))
+
+	fatherPool := x509.NewCertPool()
+
+	faterCert, err := x509.ParseCertificate(fatherCertByte)
+	if err != nil {
+		belogs.Debug("VerifyCertsByX509():fatherCertFile   ParseCertificate err:", err)
+		return "fail", err
+	}
+	faterCert.UnhandledCriticalExtensions = make([]asn1.ObjectIdentifier, 0)
+	belogs.Debug("VerifyCertsByX509():father issuer:", faterCert.Issuer.String(), "   subject:", faterCert.Subject.String())
+	fatherPool.AddCert(faterCert)
+
+	childCert, err := x509.ParseCertificate(childCertByte)
+	if err != nil {
+		belogs.Debug("VerifyCertsByX509():childCertFile:  ParseCertificate err:", err)
+		return "fail", err
+	}
+	childCert.UnhandledCriticalExtensions = make([]asn1.ObjectIdentifier, 0)
+	belogs.Debug("VerifyCertsByX509():child issuer:", childCert.Issuer.String(), "   childCert:", childCert.Subject.String())
+
+	opts := x509.VerifyOptions{
+		Roots: fatherPool,
+		//Intermediates: inter,
+		KeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageAny},
+		//KeyUsages: []x509.ExtKeyUsage{x509.KeyUsageCertSign},
+	}
+	if _, err := childCert.Verify(opts); err != nil {
+		belogs.Debug("VerifyCertsByX509():Verify err:", err)
+		return "fail", err
+	}
+	return "ok", nil
+}
+
 func VerifyCertsByX509(fatherCertFile string, childCertFile string) (result string, err error) {
 	belogs.Debug("VerifyCertsByX509():fatherCertFile:", fatherCertFile, "   childCertFile:", childCertFile)
 
--- cert/cert_test.go
+++ cert/cert_test.go
@@ -5,11 +5,29 @@ import (
 	"testing"
 )
 
-func TestXYZ(t *testing.T) {
+func TestVerifyCertByX509(t *testing.T) {
 	path := `G:\Download\cert\verify\2\`
 	fatherFile := path + `inter.cer`
 	childFile := path + `A9.cer`
 
-	result, err := VerifyCertsByX509(fatherFile, childFile)
+	result, err := VerifyCertByX509(fatherFile, childFile)
+	fmt.Println(result, err)
+}
+
+func TestVerifyEeCertByX509(t *testing.T) {
+	/*/root/rpki/repo/repo/rpki.ripe.net/repository/DEFAULT/ec/49c449-2d9c-4fc9-b340-51a23ddb6410/1/
+	rtpKuIKhDn9Y8Zg6y9HhlQfmPsU.roa
+		"eeStart": 159,
+		"eeEnd": 1426
+
+
+		/root/rpki/repo/repo/rpki.ripe.net/repository/DEFAULT/
+		ACBRR9OW8JgDvUcuWBka9usiwvU.cer
+	*/
+	path := `G:\Download\cert\verify\3\`
+	fatherFile := path + `ACBRR9OW8JgDvUcuWBka9usiwvU.cer`
+	childFile := path + `rtpKuIKhDn9Y8Zg6y9HhlQfmPsU.roa`
+
+	result, err := VerifyEeCertByX509(fatherFile, childFile, 159, 1426)
 	fmt.Println(result, err)
 }
