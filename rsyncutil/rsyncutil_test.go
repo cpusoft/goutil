d01cfa6789d0746587e27de30284a9c21d684814
cpusoft <cpusoft@live.com>
2019-10-22 18:07:29 +0800

    add

--- rsyncutil/rsyncutil_test.go
+++ rsyncutil/rsyncutil_test.go
@@ -9,8 +9,8 @@ import (
 //logpath=G:\Download\cert\log
 func TestRsyncToLogFile(t *testing.T) {
 	rsyncUrl := "http://rpki.apnic.net/repository/"
-	destpath := "/tmp/cer/"
-	logpath := "/tmp/log/"
-	rsyncDestPath, rsyncLogFile, err := RsyncToLogFile(rsyncUrl, destPath, logpath)
+	destPath := "/tmp/cer/"
+	logPath := "/tmp/log/"
+	rsyncDestPath, rsyncLogFile, err := RsyncToLogFile(rsyncUrl, destPath, logPath)
 	fmt.Println(rsyncDestPath, rsyncLogFile, err)
 }
