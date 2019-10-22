c8c70a434c311815e9e6923b1ed26419b634f061
cpusoft <cpusoft@live.com>
2019-10-22 18:05:23 +0800

    add

--- rsyncutil/rsyncutil.go
+++ rsyncutil/rsyncutil.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	belogs "github.com/astaxie/beego/logs"
+	fileutil "github.com/cpusoft/goutil/fileutil"
 	jsonutil "github.com/cpusoft/goutil/jsonutil"
 	osutil "github.com/cpusoft/goutil/osutil"
 	urlutil "github.com/cpusoft/goutil/urlutil"
@@ -22,7 +23,8 @@ const (
 	RSYNC_TYPE_IGNORE    = "ignore"
 	RSYNC_TYPE_JUST_SYNC = "justsync" //The file itself is not updated, just used to trigger sync sub-dir , so no need save to db
 
-	RSYNC_LOG_PREFIX = 12
+	RSYNC_LOG_PREFIX    = 12
+	RSYNC_LOG_FILE_NAME = "rsync.log"
 )
 
 type RsyncRecord struct {
@@ -48,174 +50,247 @@ type RsyncResult struct {
 
 // set rsync url and local dest path , then will call rsync
 // will get all stdout to get every file rsync state
-func Rsync(rsyncUrl string, destPath string) ([]RsyncResult, error) {
-	belogs.Debug("Rsync():rsyncUrl:", rsyncUrl, " destPath:", destPath)
-
-	rsyncResults := make([]RsyncResult, 0)
+// and output will save to logPath
+func RsyncToLogFile(rsyncUrl string, destPath string, logPath string) (rsyncDestPath, rsyncLogFile string, err error) {
+	belogs.Debug("RsyncToLogFile():rsyncUrl:", rsyncUrl, " destPath:", destPath, " logPath:", logPath)
 
 	// get host+path by url
 	hostAndPath, err := urlutil.HostAndPath(rsyncUrl)
-	belogs.Debug("Rsync():HostAndPath: rsyncUrl:", rsyncUrl, "  HostAndPath:", hostAndPath)
+	belogs.Debug("RsyncToLogFile():HostAndPath: rsyncUrl:", rsyncUrl, "  HostAndPath:", hostAndPath)
 	if err != nil {
-		belogs.Error("Rsync():HostAndPath: rsyncUrl:", rsyncUrl, "  HostAndPath:", hostAndPath, " err:", err)
-		return rsyncResults, err
+		belogs.Error("RsyncToLogFile():HostAndPath: rsyncUrl:", rsyncUrl, "  HostAndPath:", hostAndPath, " err:", err)
+		return "", "", err
 	}
 
 	// mkdirAll path
-	rsyncDestPath := destPath + hostAndPath
-	belogs.Debug("Rsync():rsyncDestPath:", rsyncDestPath)
+	rsyncDestPath = osutil.JoinPathFile(destPath, hostAndPath)
+	belogs.Debug("RsyncToLogFile():rsyncDestPath:", rsyncDestPath)
 	err = os.MkdirAll(rsyncDestPath, os.ModePerm)
 	if err != nil {
-		belogs.Error("Rsync():MkdirAll:", rsyncDestPath, " err:", err)
-		return rsyncResults, err
+		belogs.Error("RsyncToLogFile():MkdirAll rsyncDestPath:", rsyncDestPath, " err:", err)
+		return "", "", err
+	}
+
+	// mkdirAll path
+	rsyncLogPath := osutil.JoinPathFile(logPath, hostAndPath)
+	belogs.Debug("RsyncToLogFile():rsyncLogPath:", rsyncLogPath)
+	err = os.MkdirAll(rsyncLogPath, os.ModePerm)
+	if err != nil {
+		belogs.Error("RsyncToLogFile():MkdirAll rsyncLogPath:", rsyncLogPath, " err:", err)
+		return "", "", err
+	}
+	rsyncLogFile = osutil.JoinPathFile(rsyncLogPath, RSYNC_LOG_FILE_NAME)
+	belogs.Debug("RsyncToLogFile():rsyncLogFile:", rsyncLogFile)
+
+	// call rsync
+	//rsync -Lirzts --del --timeout=5 --contimeout=5 --no-motd  -4 rsync://rpki.afrinic.net/repository/afrinic/  /tmp/rpki.afrinic.net/repository/afrinic/
+	belogs.Debug("RsyncToLogFile(): Command: rsync", "-Lirzts", "--del", "--timeout=15", "--contimeout=15", "--no-motd", "-4", "--log-file=\""+rsyncLogFile+"\"", rsyncUrl, rsyncDestPath)
+	cmd := exec.Command("rsync", "-Lirzts", "--del", "--timeout=15", "--contimeout=15", "--no-motd", "-4", "--log-file=\""+rsyncLogFile+"\"", rsyncUrl, rsyncDestPath)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		belogs.Alert("RsyncToLogFile(): exec.Command fail, rsyncUrl is :", rsyncUrl, "   output is ", string(output), " err is :", err)
+		return "", "", err
+	}
+	belogs.Debug("RsyncToLogFile(): rsyncDestPath:", rsyncDestPath, "  rsyncLogFile:", rsyncLogFile)
+	return rsyncDestPath, rsyncLogFile, nil
+
+}
+
+// set rsync url and local dest path , then will call rsync
+// will get all stdout to get every file rsync state
+// and output will convert to rsyncResult
+func RsyncToStdout(rsyncUrl string, destPath string) (rsyncDestPath string, output []byte, err error) {
+	belogs.Debug("RsyncToStdout():rsyncUrl:", rsyncUrl, " destPath:", destPath)
+
+	//
+	output = make([]byte, 0)
+
+	// get host+path by url
+	hostAndPath, err := urlutil.HostAndPath(rsyncUrl)
+	belogs.Debug("RsyncToStdout():HostAndPath: rsyncUrl:", rsyncUrl, "  HostAndPath:", hostAndPath)
+	if err != nil {
+		belogs.Error("RsyncToStdout():HostAndPath: rsyncUrl:", rsyncUrl, "  HostAndPath:", hostAndPath, " err:", err)
+		return "", output, err
+	}
+
+	// mkdirAll path
+	rsyncDestPath = osutil.JoinPathFile(destPath, hostAndPath)
+	belogs.Debug("RsyncToStdout():rsyncDestPath:", rsyncDestPath)
+	err = os.MkdirAll(rsyncDestPath, os.ModePerm)
+	if err != nil {
+		belogs.Error("RsyncToStdout():MkdirAll:", rsyncDestPath, " err:", err)
+		return "", output, err
 	}
 
 	// call rsync
 	//rsync -Lirzts --del --timeout=5 --contimeout=5 --no-motd  -4 rsync://rpki.afrinic.net/repository/afrinic/  /tmp/rpki.afrinic.net/repository/afrinic/
-	belogs.Debug("Rsync(): Command: rsync", "-Lirzts", "--del", "--timeout=15", "--contimeout=15", "--no-motd", "-4", rsyncUrl, rsyncDestPath)
+	belogs.Debug("RsyncToStdout(): Command: rsync", "-Lirzts", "--del", "--timeout=15", "--contimeout=15", "--no-motd", "-4", rsyncUrl, rsyncDestPath)
 	cmd := exec.Command("rsync", "-Lirzts", "--del", "--timeout=15", "--contimeout=15", "--no-motd", "-4", rsyncUrl, rsyncDestPath)
-	output, err := cmd.CombinedOutput()
+	output, err = cmd.CombinedOutput()
 	if err != nil {
-		belogs.Alert("Rsync(): exec.Command fail, rsyncUrl is :", rsyncUrl, "   output is ", string(output), " err is :", err)
-		return nil, err
+		belogs.Alert("RsyncToStdout(): exec.Command fail, rsyncUrl is :", rsyncUrl, "   output is ", string(output), " err is :", err)
+		return "", output, err
 	}
-
-	// if some changed ,so length of output is > 0
-	if len(output) > 0 {
-		result := string(output)
-		results := strings.Split(result, osutil.GetNewLineSep())
-		belogs.Debug("Rsync(): len(results):", len(results))
-		for _, one := range results {
-			if len(one) <= RSYNC_LOG_PREFIX {
-				continue
-			}
-			one = strings.Replace(one, "\n", "", -1)
-			one = strings.Replace(one, "\r", "", -1)
-			rsyncResult, err := parseRsyncResult(rsyncDestPath, one)
-			if err != nil {
-				belogs.Error("Rsync(): parseRsyncResult: err: ", err, ": "+one)
-				return rsyncResults, err
-			}
-			if rsyncResult.RsyncType == RSYNC_TYPE_IGNORE ||
-				len(rsyncResult.FileName) == 0 || len(rsyncResult.FilePath) == 0 {
-				belogs.Info("Rsync():ignore parseRsyncResult: ", rsyncResult)
-				continue
-			}
-			rsyncResult.RsyncUrl = rsyncUrl
-			rsyncResults = append(rsyncResults, rsyncResult)
-		}
-	}
-
-	// then , need read all current existed cer file, to just to trigger sub ca repo sync
-	m := make(map[string]string, 0)
-	m[".cer"] = ".cer"
-	files, err := osutil.GetFilesInDir(rsyncDestPath, m)
-	belogs.Debug("Rsync():GetFilesInDir, files:", files, err)
-	if err == nil {
-		for _, file := range files {
-			belogs.Debug("Rsync():file:", file)
-			found := false
-			// if this file had synced, should not repeated add
-			for _, rsyncResult := range rsyncResults {
-				if file == rsyncResult.FileName {
-					found = true
-					break
-				}
-
-			}
-			belogs.Debug("Rsync():GetFilesInDir,found:", found, "   file:", file)
-			if !found {
-				rsyncResult := RsyncResult{}
-				rsyncResult.RsyncType = RSYNC_TYPE_JUST_SYNC
-				rsyncResult.FilePath = rsyncDestPath
-				rsyncResult.FileName = file
-				rsyncResult.FileType = "cer"
-				rsyncResult.SyncTime = time.Now()
-				rsyncResult.IsDir, _ = osutil.IsDir(file)
-				rsyncResults = append(rsyncResults, rsyncResult)
-				belogs.Info("Rsync(): manual add rsyncResult:", jsonutil.MarshalJson(rsyncResult))
-			}
-		}
-	}
-	return rsyncResults, nil
+	belogs.Debug("RsyncToStdout(): rsyncDestPath:", rsyncDestPath, "  output:", string(output))
+	return rsyncDestPath, output, nil
 }
 
-func parseRsyncResult(destPath, result string) (RsyncResult, error) {
-	belogs.Debug("parseRsyncResult():destPath:", destPath, " result:", result)
-
-	if !strings.HasSuffix(destPath, string(os.PathSeparator)) {
-		destPath = destPath + string(os.PathSeparator)
+// get results from logfile
+func ParseLogfileToRsyncResults(rsyncUrl, rsyncDestPath, rsyncLogFile string) (rsyncResults []RsyncResult, err error) {
+	// if some changed ,so length of output is > 0
+	rsyncResults = make([]RsyncResult, 0)
+	lines, err := fileutil.ReadFileToLines(rsyncLogFile)
+	if err != nil {
+		belogs.Error("ParseLogfileToRsyncResults(): ReadFileToLines: err: ", rsyncLogFile, err)
+		return rsyncResults, err
 	}
-	first := result[0]
-	second := result[1]
-	rsyncResult := RsyncResult{}
-	/*
+	if len(lines) == 0 {
+		return rsyncResults, nil
+	}
+	results := make([]string, 0)
+	for _, one := range lines {
+		pos := strings.Index(one, "]")
+		one = string(one[pos+2:])
+		results = append(results, one)
+	}
 
-		* : delete
-		< : ignore
-		> :
-		    ++++: add
-		    ....: update
-		L:  link  , ignore
-		h:  link  , ignore
-		c:
-			d: directory,    cd+++++++++
-			L: link ,cL  , ignore
-		.: attribute change ,ignore
-	*/
+	belogs.Debug("ParseLogfileToRsyncResults(): len(results):", len(results))
+	return parseToRsyncResults(rsyncUrl, rsyncDestPath, results)
+}
 
-	switch first {
-	case '*':
-		rsyncResult.RsyncType = RSYNC_TYPE_DEL
-		rsyncResult.FilePath, rsyncResult.FileName =
-			osutil.GetFilePathAndFileName(destPath + string(result[RSYNC_LOG_PREFIX:]))
-		rsyncResult.FileType = strings.Replace(path.Ext(rsyncResult.FileName), ".", "", -1)
-	case '>':
-		if strings.Contains(result, "++++") {
-			rsyncResult.RsyncType = RSYNC_TYPE_ADD
-		} else {
-			rsyncResult.RsyncType = RSYNC_TYPE_UPDATE
+// get results from stdout
+func ParseStdoutToRsyncResults(rsyncUrl string, rsyncDestPath string, output []byte) (rsyncResults []RsyncResult, err error) {
+	// if some changed ,so length of output is > 0
+	if len(output) == 0 {
+		return rsyncResults, nil
+	}
+	result := string(output)
+	results := strings.Split(result, osutil.GetNewLineSep())
+	belogs.Debug("ParseStdoutToRsyncResults(): len(results):", len(results))
+	return parseToRsyncResults(rsyncUrl, rsyncDestPath, results)
+
+}
+
+func parseToRsyncResults(rsyncUrl string, rsyncDestPath string, results []string) (rsyncResults []RsyncResult, err error) {
+	belogs.Debug("parseToRsyncResults():destPath:", rsyncDestPath, " result:", results)
+	rsyncResults = make([]RsyncResult, 0)
+	if !strings.HasSuffix(rsyncDestPath, string(os.PathSeparator)) {
+		rsyncDestPath = rsyncDestPath + string(os.PathSeparator)
+	}
+	for _, result := range results {
+		if len(result) <= RSYNC_LOG_PREFIX {
+			continue
 		}
-		rsyncResult.FilePath, rsyncResult.FileName =
-			osutil.GetFilePathAndFileName(destPath + string(result[RSYNC_LOG_PREFIX:]))
-		rsyncResult.FileType = strings.Replace(path.Ext(rsyncResult.FileName), ".", "", -1)
-	case 'c':
-		switch second {
-		case 'd':
-			rsyncResult.RsyncType = RSYNC_TYPE_MKDIR
-			rsyncResult.FilePath = destPath + string(result[RSYNC_LOG_PREFIX:])
+		result = strings.Replace(result, "\n", "", -1)
+		result = strings.Replace(result, "\r", "", -1)
+		first := result[0]
+		second := result[1]
+		rsyncResult := RsyncResult{}
+		/*
+			https://stackoverflow.com/questions/40612505/format-of-rsync-logfile
+					* : delete
+					< : ignore
+					> :
+					    ++++: add
+					    ....: update
+					L:  link  , ignore
+					h:  link  , ignore
+					c:
+						d: directory,    cd+++++++++
+						L: link ,cL  , ignore
+					.: attribute change ,ignore
+		*/
 
+		switch first {
+		case '*':
+			rsyncResult.RsyncType = RSYNC_TYPE_DEL
+			rsyncResult.FilePath, rsyncResult.FileName =
+				osutil.GetFilePathAndFileName(rsyncDestPath + string(result[RSYNC_LOG_PREFIX:]))
+			rsyncResult.FileType = strings.Replace(path.Ext(rsyncResult.FileName), ".", "", -1)
+		case '>':
+			if strings.Contains(result, "++++") {
+				rsyncResult.RsyncType = RSYNC_TYPE_ADD
+			} else {
+				rsyncResult.RsyncType = RSYNC_TYPE_UPDATE
+			}
+			rsyncResult.FilePath, rsyncResult.FileName =
+				osutil.GetFilePathAndFileName(rsyncDestPath + string(result[RSYNC_LOG_PREFIX:]))
+			rsyncResult.FileType = strings.Replace(path.Ext(rsyncResult.FileName), ".", "", -1)
+		case 'c':
+			switch second {
+			case 'd':
+				rsyncResult.RsyncType = RSYNC_TYPE_MKDIR
+				rsyncResult.FilePath = rsyncDestPath + string(result[RSYNC_LOG_PREFIX:])
+
+			default:
+				rsyncResult.RsyncType = RSYNC_TYPE_IGNORE
+			}
+		case '<':
+			fallthrough
+		case 'L':
+			fallthrough
+		case 'h':
+			fallthrough
+		case '.':
+			fallthrough
 		default:
 			rsyncResult.RsyncType = RSYNC_TYPE_IGNORE
 		}
-	case '<':
-		fallthrough
-	case 'L':
-		fallthrough
-	case 'h':
-		fallthrough
-	case '.':
-		fallthrough
-	default:
-		rsyncResult.RsyncType = RSYNC_TYPE_IGNORE
+		rsyncResult.SyncTime = time.Now()
+		rsyncResult.IsDir, _ = osutil.IsDir(rsyncResult.FilePath + rsyncResult.FileName)
+		belogs.Info("parseToRsyncResults():rsyncResult:", jsonutil.MarshalJson(rsyncResult))
+		if err != nil {
+			belogs.Error("parseToRsyncResults(): parseRsyncResult: err: ", err, ": "+result)
+			return rsyncResults, err
+		}
+		if rsyncResult.RsyncType == RSYNC_TYPE_IGNORE ||
+			len(rsyncResult.FileName) == 0 || len(rsyncResult.FilePath) == 0 {
+			belogs.Debug("parseToRsyncResults():ignore parseRsyncResult: ", rsyncResult)
+			continue
+		}
+		rsyncResult.RsyncUrl = rsyncUrl
+		rsyncResults = append(rsyncResults, rsyncResult)
 	}
-	rsyncResult.SyncTime = time.Now()
-	rsyncResult.IsDir, _ = osutil.IsDir(rsyncResult.FilePath + rsyncResult.FileName)
-	belogs.Info("parseRsyncResult():rsyncResult:", jsonutil.MarshalJson(rsyncResult))
-	return rsyncResult, nil
+	belogs.Debug("parseToRsyncResults():rsyncResults: ", jsonutil.MarshalJson(rsyncResults))
+	return rsyncResults, nil
 
 }
 
-// only rsync local files, include file extension
-func RsyncLocalIncludeFileExt(srcPath string, destPath string, includeFileExt string) error {
-	//rsync -Lrts --del  --no-motd --include="*.roa" --include="*/"  --exclude="*" /root/rpki/repo/repo /root/rpki/repo/repo-1
-	belogs.Debug("RsyncLocalIncludeFileExt(): Command: rsync", "-Lrts", "--del", "--no-motd", "--include="+includeFileExt,
-		"--include=*/", "--exclude=*", srcPath, destPath)
-	cmd := exec.Command("rsync", "-Lrts", "--del", "--no-motd", "--include="+includeFileExt, "--include=*/", "--exclude=*", srcPath, destPath)
-	_, err := cmd.CombinedOutput()
+//  need read all current existed cer file, to just to trigger sub ca repo sync
+func AddCerToRsyncResults(rsyncDestPath string, rsyncResults []RsyncResult) (err error) {
+
+	m := make(map[string]string, 0)
+	m[".cer"] = ".cer"
+	files, err := osutil.GetFilesInDir(rsyncDestPath, m)
+	belogs.Debug("addCerToRsyncResults():GetFilesInDir, files:", files, err)
 	if err != nil {
-		belogs.Alert("RsyncLocalIncludeFileExt(): exec.Command fail, srcPath is :", srcPath, "   destPath is ", destPath, " err is :", err)
+		belogs.Debug("addCerToRsyncResults():GetFilesInDir, files:", files, err)
 		return err
 	}
+	for _, file := range files {
+		belogs.Debug("addCerToRsyncResults():file:", file)
+		found := false
+		// if this file had synced, should not repeated add
+		for _, rsyncResult := range rsyncResults {
+			if file == rsyncResult.FileName {
+				found = true
+				break
+			}
+
+		}
+		belogs.Debug("addCerToRsyncResults():GetFilesInDir,found:", found, "   file:", file)
+		if !found {
+			rsyncResult := RsyncResult{}
+			rsyncResult.RsyncType = RSYNC_TYPE_JUST_SYNC
+			rsyncResult.FilePath = rsyncDestPath
+			rsyncResult.FileName = file
+			rsyncResult.FileType = "cer"
+			rsyncResult.SyncTime = time.Now()
+			rsyncResult.IsDir, _ = osutil.IsDir(file)
+			rsyncResults = append(rsyncResults, rsyncResult)
+			belogs.Info("addCerToRsyncResults(): manual add rsyncResult:", jsonutil.MarshalJson(rsyncResult))
+		}
+	}
 	return nil
+
 }
--- rsyncutil/rsyncutil_test.go
+++ rsyncutil/rsyncutil_test.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 )
 
-func TestRsyncLocalIncludeFileExt(t *testing.T) {
-	srcPath := "/root/rpki/repo/repo"
-	destPath := "/root/rpki/repo/repo-1"
-	includeFileExt := "*.roa"
-	err := RsyncLocalIncludeFileExt(srcPath, destPath, includeFileExt)
-	fmt.Println(err)
+//destpath=G:\Download\cert\rsync
+//logpath=G:\Download\cert\log
+func TestRsyncToLogFile(t *testing.T) {
+	rsyncUrl := "http://rpki.apnic.net/repository/"
+	destpath := "/tmp/cer/"
+	logpath := "/tmp/log/"
+	rsyncDestPath, rsyncLogFile, err := RsyncToLogFile(rsyncUrl, destPath, logpath)
+	fmt.Println(rsyncDestPath, rsyncLogFile, err)
 }
