49cee1a36e42664f3f4265575d0eb5b4317bd1a8
cpusoft <cpusoft@live.com>
2019-10-31 10:35:50 +0800

    add

--- rsyncutil/rsyncutil.go
+++ rsyncutil/rsyncutil.go
@@ -149,8 +149,8 @@ func RsyncToStdout(rsyncUrl string, destPath string) (rsyncDestPath string, outp
 
 	// call rsync
 	//rsync -Lirzts --del --timeout=5 --contimeout=5 --no-motd  -4 rsync://rpki.afrinic.net/repository/afrinic/  /tmp/rpki.afrinic.net/repository/afrinic/
-	belogs.Debug("RsyncToStdout(): Command: rsync", "-Lirzts", "--del", "--no-motd", "-4", rsyncUrl, rsyncDestPath)
-	cmd := exec.Command("rsync", "-Lirzts", "--del", "--no-motd", "-4", rsyncUrl, rsyncDestPath)
+	belogs.Debug("RsyncToStdout(): Command: rsync", "-Lirzts", "--del", "--timeout=600", "--no-motd", "-4", rsyncUrl, rsyncDestPath)
+	cmd := exec.Command("rsync", "-Lirzts", "--del", "--timeout=600", "--no-motd", "-4", rsyncUrl, rsyncDestPath)
 	output, err = cmd.CombinedOutput()
 	if err != nil {
 		belogs.Alert("RsyncToStdout(): exec.Command fail, rsyncUrl is :", rsyncUrl, "   output is ", string(output), " err is :", err)
