7919a402a80ec8cfbc49b165fcc4e25899ac4299
cpusoft <cpusoft@live.com>
2019-11-13 11:13:26 +0800

    add

--- asnutil/asnutil.go
+++ asnutil/asnutil.go
@@ -3,12 +3,19 @@ package asnutil
 import ()
 
 // asn is included in parent asn ;  .
-// selfMin, selfMax, parentMin, parentMax
-func AsnIncludeInParentAsn(selfMin, selfMax, parentMin, parentMax uint64) bool {
+// selfAsn, selfMin, selfMax,parentAsn, parentMin, parentMax
+func AsnIncludeInParentAsn(selfAsn, selfMin, selfMax, parentAsn, parentMin, parentMax uint64) bool {
 
-	if selfMin == 0 || selfMax == 0 ||
-		parentMin == 0 || parentMax == 0 {
+	if selfAsn == 0 && selfMin == 0 && selfMax == 0 &&
+		parentAsn == 0 && parentMin == 0 && parentMax == 0 {
 		return false
+	}
+
+	if selfAsn == parentAsn {
+		return true
+	}
+	if parentMin <= selfAsn && selfAsn <= parentMax {
+		return true
 	}
 
 	// parentMin <--- selfMin <---------> selfMax ---> parentMax
