0d9380e62e885b02e5ff1a20629a145276cfd24d
cpusoft <cpusoft@live.com>
2019-11-28 09:51:32 +0800

    add

--- randutil/randutil.go
+++ randutil/randutil.go
@@ -9,3 +9,8 @@ func Intn(n uint) int {
 	rand.Seed(time.Now().Unix())
 	return rand.Intn(int(n))
 }
+
+func IntRange(min, n uint) int {
+	rand.Seed(time.Now().Unix())
+	return int(min) + rand.Intn(int(n))
+}
