4c5cb85318debcfa7a7c4f41ab13a6d90822aa07
cpusoft <cpusoft@live.com>
2020-05-17 16:56:11 +0800

    add

--- ginserver/ginsession.go
+++ ginserver/ginsession.go
@@ -27,6 +27,9 @@ func InitSessionInMem(engine *gin.Engine, cookieName string, maxAge int, secure,
 }
 
 func SaveToSession(c *gin.Context, key string, value interface{}) error {
+	if len(key) == 0 || value == nil {
+		return nil
+	}
 	s := sessions.Default(c)
 	s.Set(key, jsonutil.MarshalJson(value))
 	return s.Save()
@@ -35,14 +38,28 @@ func SaveUserToSession(c *gin.Context, ginUserModel *GinUserModel) error {
 	return SaveToSession(c, "ginuser", ginUserModel)
 }
 func GetFromSession(c *gin.Context, key string, value interface{}) error {
+	if len(key) == 0 {
+		return nil
+	}
+
 	s := sessions.Default(c)
+	if s == nil {
+		return nil
+	}
 	v := s.Get(key)
+	if v == nil {
+		return nil
+	}
 	return jsonutil.UnmarshalJson(v.(string), &value)
 }
 func GetUserFromSession(c *gin.Context, ginUserModel *GinUserModel) error {
 	return GetFromSession(c, "ginuser", ginUserModel)
 }
 func DeleteInSession(c *gin.Context, key string) {
+	if len(key) == 0 {
+		return
+	}
+
 	s := sessions.Default(c)
 	s.Delete(key)
 	s.Save()
