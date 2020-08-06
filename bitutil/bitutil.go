9ba202fd59561e7ba62e818619d1f1915cadc9ff
cpusoft <cpusoft@live.com>
2020-08-06 16:13:47 +0800

    add

---
+++ bitutil/bitutil.go
@@ -0,0 +1,23 @@
+package bitutil
+
+import ()
+
+// 0x01(0000 0001) --> left 2 --> 0x(0000 0111)
+// rfc3779 max : usine |, right bits are one
+func LeftAndFillOne(bits uint8) (a uint8) {
+	a = 1
+	for i := uint8(0); i < bits; i++ {
+		a = a | a<<1
+	}
+	return a
+}
+
+// 0x01(1111 1111) --> left 6 --> 0x(1100 0000)
+// rfc3779 min : usine &, right bits are zero
+func LeftAndFillZero(bits uint8) (a uint8) {
+	a = 1
+	for i := uint8(0); i < bits; i++ {
+		a = a << 1
+	}
+	return a
+}
---
+++ bitutil/bitutil_test.go
@@ -0,0 +1,18 @@
+package bitutil
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestXYZ(t *testing.T) {
+	var z uint8 = 0x80
+	a := LeftAndFillOne(3)
+	fmt.Printf("%x,%d,%b\n", a, a, a)
+	z = z | a
+	fmt.Printf("%x,%d,%b\n", z, z, z)
+
+	z = 0xff
+	a := LeftAndFillOne(3)
+
+}
