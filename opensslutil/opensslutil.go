11bfa0975c75383ff34f8660a3d9a0686358a727
cpusoft <cpusoft@live.com>
2020-09-16 11:34:29 +0800

    add

---
+++ opensslutil/opensslutil.go
@@ -0,0 +1,41 @@
+package opensslutil
+
+import (
+	"errors"
+	"os/exec"
+	"strings"
+
+	belogs "github.com/astaxie/beego/logs"
+	osutil "github.com/cpusoft/goutil/osutil"
+)
+
+func GetResultsByOpensslX509(certFile string) (results []string, err error) {
+	belogs.Debug("GetResultsByOpensslX509(): cmd:  openssl", "x509", "-noout", "-text", "-in", certFile, "--inform", "der")
+	cmd := exec.Command("openssl", "x509", "-noout", "-text", "-in", certFile, "--inform", "der")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		belogs.Error("GetResultsByOpensslX509(): exec.Command: certFile:", certFile, "   err: ", err, "   output: "+string(output))
+		return nil, errors.New(string(output) + ". " + err.Error())
+	}
+	result := string(output)
+	results = strings.Split(result, osutil.GetNewLineSep())
+	belogs.Debug("GetResultsByOpensslX509(): len(results):", len(results))
+	return results, nil
+}
+
+func GetResultsByOpensslAns1(certFile string) (results []string, err error) {
+
+	//https://blog.csdn.net/Zhymax/article/details/7683925
+	//openssl asn1parse -in -ard.mft -inform DER
+	belogs.Debug("GetResultsByOpensslAns1():cmd: openssl", "asn1parse", "-in", certFile, "--inform", "der")
+	cmd := exec.Command("openssl", "asn1parse", "-in", certFile, "--inform", "der")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		belogs.Error("GetResultsByOpensslAns1(): exec.Command: certFile:", certFile, "   err: ", err, ": "+string(output))
+		return nil, errors.New(string(output) + ". " + err.Error())
+	}
+	result := string(output)
+	results = strings.Split(result, osutil.GetNewLineSep())
+	belogs.Debug("GetResultsByOpensslAns1(): len(results):", len(results))
+	return results, nil
+}
