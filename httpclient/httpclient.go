d9331a14f8ea79568836776e43fb335d0d1b1219
cpusoft <cpusoft@live.com>
2021-03-29 17:47:58 +0800

    connect time out

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	DefaultUserAgent     = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
-	DefaultTimeout       = 30
+	DefaultTimeout       = 120
 	RetryCount           = 3
 	RetryIntervalSeconds = 5
 )
