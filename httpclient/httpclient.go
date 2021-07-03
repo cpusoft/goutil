d6655d5c8e9315f423ff5768be93d5fb47ad8789
cpusoft <cpusoft@live.com>
2021-07-04 07:36:20 +0800

    change timeout to 5 mins

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	DefaultUserAgent     = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36 RPSTIR2"
-	DefaultTimeout       = 120
+	DefaultTimeout       = 10
 	RetryCount           = 3
 	RetryIntervalSeconds = 5
 )
