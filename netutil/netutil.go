b1e068629f67f5c70859ec573fd3dc0392da3b9c
cpusoft <cpusoft@live.com>
2022-05-17 10:38:34 +0800

    net

--- netutil/netutil.go
+++ netutil/netutil.go
@@ -4,6 +4,7 @@ import (
 	"net"
 
 	"github.com/cpusoft/goutil/belogs"
+	"github.com/cpusoft/goutil/urlutil"
 )
 
 // ip network
@@ -13,7 +14,7 @@ import (
 func ResolveIp(host string) *net.IPAddr {
 	ipAddr, err := net.ResolveIPAddr("ip", host)
 	if err != nil {
-		belogs.Error("GetIpByHost():  ResolveIPAddr fail:", err)
+		belogs.Error("GetIpByHost():  ResolveIPAddr fail:", host, err)
 		return nil
 	}
 	return ipAddr
@@ -23,7 +24,21 @@ func ResolveIp(host string) *net.IPAddr {
 func LookupIp(host string) []net.IP {
 	ipAddrs, err := net.LookupIP(host)
 	if err != nil {
-		belogs.Error("GetIpByHost():  ResolveIPAddr fail:", err)
+		belogs.Error("GetIpByHost(): ResolveIPAddr fail:", host, err)
+		return nil
+	}
+	return ipAddrs
+}
+
+func LookupIpByUrl(url string) []net.IP {
+	host, err := urlutil.Host(url)
+	if err != nil {
+		belogs.Error("GetIpByHost():  Host fail:", url, err)
+		return nil
+	}
+	ipAddrs, err := net.LookupIP(host)
+	if err != nil {
+		belogs.Error("GetIpByHost():  ResolveIPAddr fail:", url, host, err)
 		return nil
 	}
 	return ipAddrs
--- netutil/netutil_test.go
+++ netutil/netutil_test.go
@@ -14,3 +14,7 @@ func TestLookupIp(t *testing.T) {
 	host := "baidu.com"
 	fmt.Println("resolveIp:", LookupIp(host))
 }
+func TestLookupIpByUrl(t *testing.T) {
+	url := "https://rrdp.lacnic.net/rrdp/26afc8dd-3e3b-4b31-8ff1-710d8e944320/22733/delta.xml"
+	fmt.Println("resolveIpByUrl:", LookupIpByUrl(url))
+}
