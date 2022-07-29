218ad9a614ec89fb89ff5e0512b3b38f9475b4d8
cpusoft <cpusoft@live.com>
2022-07-29 15:13:34 +0800

    ResourceRecordModel

--- dnsutil/dnsmodel.go
+++ dnsutil/dnsmodel.go
@@ -1,6 +1,51 @@
 package dnsutil
 
-import "github.com/cpusoft/goutil/belogs"
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/cpusoft/goutil/belogs"
+	"github.com/cpusoft/goutil/jsonutil"
+)
+
+type ResourceRecordModel struct {
+	DnsName     jsonutil.HexBytes `json:"dnsName"`  // 2*n bytes --> n uint16
+	DnsType     uint16            `json:"dnsType"`  // 'type' is keyword in golang, so use dnsType
+	DnsClass    uint16            `json:"dnsClass"` //
+	DnsTtl      uint32            `json:"dnsTtl"`
+	DnsRdLength uint16            `json:"dnsRdLength"`
+	DnsRData    jsonutil.HexBytes `json:"dnsRData"`
+}
+
+func NewResourceRecordModel(dnsName []byte, dnsType uint16, dnsClass uint16,
+	dnsTtl uint32, dnsRdLength uint16, dnsRData []byte) *ResourceRecordModel {
+	c := &ResourceRecordModel{
+		DnsName:     dnsName,
+		DnsType:     dnsType,
+		DnsClass:    dnsClass,
+		DnsTtl:      dnsTtl,
+		DnsRdLength: dnsRdLength,
+		DnsRData:    dnsRData,
+	}
+	return c
+}
+
+func (c *ResourceRecordModel) Length() uint16 {
+	// type(2)+class(2)+ttl(4)+rdlen(2)
+	return uint16(len(c.DnsName) + 2 + 2 + 4 + 2 + len(c.DnsRData))
+}
+func (c *ResourceRecordModel) Bytes() []byte {
+	wr := bytes.NewBuffer([]byte{})
+	binary.Write(wr, binary.BigEndian, c.DnsName)
+	binary.Write(wr, binary.BigEndian, c.DnsType)
+	binary.Write(wr, binary.BigEndian, c.DnsClass)
+	binary.Write(wr, binary.BigEndian, c.DnsTtl)
+	binary.Write(wr, binary.BigEndian, c.DnsRdLength)
+	if len(c.DnsRData) > 0 {
+		binary.Write(wr, binary.BigEndian, c.DnsRData)
+	}
+	return wr.Bytes()
+}
 
 type QrOpCodeZRCode uint16
 
