0a36ffa1d3d9a9735390f44ecc40309c12bda67e
cpusoft <cpusoft@live.com>
2022-07-29 13:47:30 +0800

    dnsutil dnsmodel

--- dnsutil/dnsmodel.go
+++ dnsutil/dnsmodel.go
@@ -16,12 +16,12 @@ func ComposeQrOpCodeZRCode(qr uint8, opCode uint8, rCode uint8) (qrOpCodeZRCode 
 	return qrOpCodeZRCode
 }
 
-func ParseQrOpCodeZRCode(qrOpCodeZRCode QrOpCodeZRCode) (qr uint8, opCode uint8, rCode uint8) {
+func ParseQrOpCodeZRCode(qrOpCodeZRCode QrOpCodeZRCode) (qr uint8, opCode uint8, z uint8, rCode uint8) {
 	qr = uint8((qrOpCodeZRCode >> 15) & 1)        // 1000 0000 0000 0000 --> 1
 	opCode = uint8((qrOpCodeZRCode >> 11) & 0x0f) // 0111 1000 0000 0000 --> 1111
-	z := uint8((qrOpCodeZRCode >> 4) & 0x7f)      // 0000 0111 1111 0000 --> 111 1111
+	z = uint8((qrOpCodeZRCode >> 4) & 0x7f)       // 0000 0111 1111 0000 --> 111 1111
 	rCode = uint8(qrOpCodeZRCode & 0x0f)          // 0000 0000 0000 1111 --> 1111
 	belogs.Debug("ParseQrOpCodeZRCode():qrOpCodeZRCode:", qrOpCodeZRCode, "  qr:", qr, " opCode:", opCode,
 		"  z:", z, "  rCode:", rCode)
-	return qr, opCode, rCode
+	return qr, opCode, z, rCode
 }
