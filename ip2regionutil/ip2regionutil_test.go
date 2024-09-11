e3021875d452ad133002143cb966ee91c5f45cd1
cpusoft <cpusoft@live.com>
2024-09-11 22:11:25 +0800

    ip2region

--- ip2regionutil/ip2regionutil_test.go
+++ ip2regionutil/ip2regionutil_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSearchIp2Region(t *testing.T) {
-	f := `F:\share\我的坚果云\Go\dns\research\ip2region\data\ip2region.xdb`
+	f := `./ip2region.xdb`
 	ip := `93.184.215.14 `
 	r, e := SearchIp2Region(f, ip)
 	fmt.Println(r, e)
