4c7e23611302b82a5c24dfe5bda4ab8d8af52564
baozhuo <291109232@qq.com>
2024-09-25 09:15:54 +0800

    feat:增加badgebd

---
+++ badgedb/badgedb_str_test.go
@@ -0,0 +1,31 @@
+package badgedb
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestStoreWithCompositeKey(t *testing.T) {
+	Init()
+	columns := map[string]string{
+		"name": "John",
+		"age":  "30",
+	}
+
+	err := StoreWithCompositeKey("user", "123", columns)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("User data stored.")
+	columnsToQuery := map[string]string{
+		"name": "John",
+		"age":  "30",
+	}
+
+	result, err := QueryByCompositeKey("user", columnsToQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Query result:", string(result))
+}
