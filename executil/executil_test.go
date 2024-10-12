9639acd2c0c63b23c9a82247fc65b907efc41e29
cpusoft <cpusoft@live.com>
2024-10-12 17:41:12 +0800

    test

--- executil/executil_test.go
+++ executil/executil_test.go
@@ -21,7 +21,7 @@ func TestExecCommand(t *testing.T) {
 }
 
 func TestExecCommandCombinedOutput(t *testing.T) {
-	p := `ca -gencrl -out /home/rpki/gencerts/ripencc/subcert/tmp/test.crl  -cert /home/rpki/gencerts/ripencc/subca/ripencc_subca.pem -keyfile /home/rpki/gencerts/ripencc/subca/ripencc_subca.key -config /home/rpki/gencerts/ripencc/subcert/crl.cnf -crl_lastupdate 241011010203Z -crl_nextupdate 341011010203Z`
+	p := `ca -gencrl -verbose  -out /home/rpki/gencerts/ripencc/subcert/tmp/test.crl  -cert /home/rpki/gencerts/ripencc/subca/ripencc_subca.pem -keyfile /home/rpki/gencerts/ripencc/subca/ripencc_subca.key -config /home/rpki/gencerts/ripencc/subcert/crl.cnf -crl_lastupdate 241011010203Z -crl_nextupdate 341011010203Z`
 	params := strings.Split(p, " ")
 	out, err := ExecCommandCombinedOutput("openssl", params)
 	fmt.Println("out", out)
