3c6f99d1f887ec01c43ca0ed83f7b50a27ed1504
cpusoft <cpusoft@live.com>
2024-11-15 18:52:31 +0800

    idutil

--- idutil/idgenerator.go
+++ idutil/idgenerator.go
@@ -1,79 +1,46 @@
 package idutil
 
 import (
-	"fmt"
-	"sync"
-	"time"
-
 	"github.com/bwmarrin/snowflake"
+	"github.com/cpusoft/goutil/belogs"
 )
 
-// IDGenerator 结构体
-type IDGeneratorInc struct {
-	mu sync.Mutex
-	id int
-}
-
-// NewIDGenerator 创建一个新的ID生成器
-func NewIDGeneratorInc() *IDGeneratorInc {
-	return &IDGeneratorInc{id: 0}
-}
-
-// 自增
-func (gen *IDGeneratorInc) Generate() int {
-	gen.mu.Lock()
-	defer gen.mu.Unlock()
-	gen.id++
-	return gen.id
-}
-
-// IDGeneratorBaseTime 结构体
-type IDGeneratorBaseTime struct {
-	mu       sync.Mutex
-	lastTime int64
-	sequence int
-}
-
-// NewIDGenerator 创建一个新的ID生成器
-func NewIDGeneratorBaseTime() *IDGeneratorBaseTime {
-	return &IDGeneratorBaseTime{
-		lastTime: time.Now().UnixNano() / int64(time.Millisecond),
-		sequence: 0,
-	}
-}
-
-// Generate 生成一个新的ID
-func (gen *IDGeneratorBaseTime) Generate() int64 {
-	gen.mu.Lock()
-	defer gen.mu.Unlock()
-
-	now := time.Now().UnixNano() / int64(time.Millisecond)
-
-	if now == gen.lastTime {
-		gen.sequence++
-	} else {
-		gen.lastTime = now
-		gen.sequence = 0
-	}
-
-	id := (now << 20) | int64(gen.sequence)
-	return id
-}
-
+/*
 // 定义全局的 snowflake 节点
 var node *snowflake.Node
 
 // 初始化节点
-func InitSnowflake(nodeID int64) error {
-	var err error
-	node, err = snowflake.NewNode(nodeID)
-	if err != nil {
-		return fmt.Errorf("failed to initialize snowflake node: %w", err)
+
+	func InitSnowflake(nodeID int64) error {
+		var err error
+		node, err = snowflake.NewNode(nodeID)
+		if err != nil {
+			return fmt.Errorf("failed to initialize snowflake node: %w", err)
+		}
+		return nil
 	}
-	return nil
-}
 
 // 生成自增 ID 的方法
-func GenerateID() snowflake.ID {
-	return node.Generate()
+
+	func GenerateID() snowflake.ID {
+		return node.Generate()
+	}
+*/
+
+func GenerateInt64(nodeId int64) (int64, error) {
+	node, err := snowflake.NewNode(nodeId)
+	if err != nil {
+		belogs.Error("GenerateInt64(): nodeId:", nodeId, err)
+		return 0, err
+	}
+	return node.Generate().Int64(), nil
+}
+
+func GenerateString(nodeId int64) (string, error) {
+	node, err := snowflake.NewNode(nodeId)
+	if err != nil {
+		belogs.Error("GenerateString(): nodeId:", nodeId, err)
+		return "", err
+	}
+	return node.Generate().String(), nil
 }
