c5657e18be59b1ab4d73abf096e6c6e409243025
cpusoft <cpusoft@live.com>
2025-06-22 08:32:38 +0800

    gomodup

---
+++ collectutil/collectutil.go
@@ -0,0 +1,9 @@
+package collectutil
+
+func MapValuesToSlice[K comparable, V any](m map[K]V) []V {
+	values := make([]V, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	return values
+}
