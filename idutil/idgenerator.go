58bfa7d38124eff4dbb05eb112ff6b6609f731f7
cpusoft <cpusoft@live.com>
2025-06-24 14:48:37 +0800

    idutil

--- idutil/idgenerator.go
+++ idutil/idgenerator.go
@@ -1,6 +1,9 @@
 package idutil
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/bwmarrin/snowflake"
 	"github.com/cpusoft/goutil/belogs"
 )
@@ -26,7 +29,11 @@ var node *snowflake.Node
 		return node.Generate()
 	}
 */
+func GenerateSnowflakInt64(nodeId int64) (int64, error) {
+	return GenerateInt64(nodeId)
+}
 
+// Deprecated: Use GenerateSnowflakInt64 instead.
 func GenerateInt64(nodeId int64) (int64, error) {
 	node, err := snowflake.NewNode(nodeId)
 	if err != nil {
@@ -36,6 +43,11 @@ func GenerateInt64(nodeId int64) (int64, error) {
 	return node.Generate().Int64(), nil
 }
 
+func GenerateSnowflakeString(nodeId int64) (string, error) {
+	return GenerateString(nodeId)
+}
+
+// Deprecated: Use GenerateSnowflakeString instead.
 func GenerateString(nodeId int64) (string, error) {
 	node, err := snowflake.NewNode(nodeId)
 	if err != nil {
@@ -43,4 +55,11 @@ func GenerateString(nodeId int64) (string, error) {
 		return "", err
 	}
 	return node.Generate().String(), nil
+}
+
+// GenerateSequentialUint64UUID generates a uint64 based on timestamp and randomness.
+func GenerateSequentialUint64UUID() uint64 {
+	ts := uint64(time.Now().UnixNano())  // Current time in nanoseconds
+	r := uint64(rand.Int63())            // Random 63-bit number
+	return (ts << 32) | (r & 0xFFFFFFFF) // Combine time and random bits
 }
