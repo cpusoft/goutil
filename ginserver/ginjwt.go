ee7a3379888e63bcab3609ba2c645d9998f7bf54
cpusoft <cpusoft@live.com>
2025-10-22 10:42:13 +0800

    ginjwt mv to ginserver from ginsession

---
+++ ginserver/ginjwt.go
@@ -0,0 +1,106 @@
+package ginserver
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/cpusoft/goutil/belogs"
+	"github.com/cpusoft/goutil/conf"
+	"github.com/cpusoft/goutil/jsonutil"
+	"github.com/cpusoft/goutil/jwtutil"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	JWT_HEADER_AUTHORIZATION   = "Authorization"
+	JWT_HEADER_PREFIX_BEARER   = "Bearer"
+	JWT_CTX_CustomClaims_Infos = "CustomClaims.Infos"
+)
+
+// ginsession.RegisterJwt(engine)
+func EngineRegisterJwt(engine *gin.Engine) {
+	engine.Use(jwtAuthMiddleware())
+}
+func RouterGroupRegisterJwt(group *gin.RouterGroup) {
+	group.Use(jwtAuthMiddleware())
+}
+
+// JWT中间件：验证令牌并将用户信息存入上下文
+func jwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 从请求头中获取Authorization字段
+		authHeader := c.GetHeader(JWT_HEADER_AUTHORIZATION)
+		if authHeader == "" {
+			ResponseFail(c, errors.New("Authorization is empty"), nil)
+			c.Abort()
+			return
+		}
+		belogs.Debug("jwtAuthMiddleware(): authHeader:", authHeader)
+
+		// 验证Authorization格式（Bearer <token>）
+		var tokenString string
+		_, err := fmt.Sscanf(authHeader, JWT_HEADER_PREFIX_BEARER+" %s", &tokenString)
+		if err != nil {
+			belogs.Error("jwtAuthMiddleware(): Sscanf tokenString fail, authHeader:", authHeader, err)
+			ResponseFail(c, err, nil)
+			c.Abort()
+			return
+		}
+		belogs.Debug("jwtAuthMiddleware(): tokenString:", tokenString)
+
+		customClaims, err := jwtutil.ParseToken(tokenString, conf.String("jwt::secret"))
+		if err != nil {
+			belogs.Error("jwtAuthMiddleware(): ParseToken fail, tokenString:", tokenString, err)
+			ResponseFail(c, err, nil)
+			c.Abort()
+			return
+		}
+		belogs.Debug("jwtAuthMiddleware(): customClaims:", jsonutil.MarshalJson(customClaims))
+		c.Set(JWT_CTX_CustomClaims_Infos, customClaims.Infos)
+		c.Next()
+	}
+}
+
+func SetToContextWithValue(c *gin.Context) context.Context {
+	m, exists := c.Get(JWT_CTX_CustomClaims_Infos)
+	if !exists {
+		belogs.Error("SetToContextWithValue(): get JWT_CTX_CustomClaims_Infos from gin.Context fail, JWT_CTX_CustomClaims_Infos:", JWT_CTX_CustomClaims_Infos)
+		return nil
+	}
+	belogs.Debug("SetToContextWithValue(): get JWT_CTX_CustomClaims_Infos", "m", jsonutil.MarshalJson(m))
+	authHeader := c.GetHeader(JWT_HEADER_AUTHORIZATION)
+	ctx := context.WithValue(context.Background(), JWT_CTX_CustomClaims_Infos, m)
+	ctx = context.WithValue(ctx, JWT_HEADER_AUTHORIZATION, authHeader)
+	return ctx
+}
+
+func GetCustomClaims(ctx context.Context) map[string]interface{} {
+	cc := ctx.Value(JWT_CTX_CustomClaims_Infos)
+	if cc == nil {
+		belogs.Error("GetCustomClaims(): get JWT_CTX_CustomClaims_Infos from gin.Context fail",
+			"JWT_CTX_CustomClaims_Infos:", JWT_CTX_CustomClaims_Infos)
+		return make(map[string]interface{})
+	}
+	m, ok := cc.(map[string]interface{})
+	if !ok {
+		belogs.Error("GetCustomClaims(): assert to CustomClaims fail, cc:", jsonutil.MarshalJson(cc))
+		return make(map[string]interface{})
+	}
+	return m
+}
+
+func GetAuthHeader(ctx context.Context) string {
+	authHeader := ctx.Value(JWT_HEADER_AUTHORIZATION)
+	if authHeader == nil {
+		belogs.Error("GetAuthHeader(): get JWT_HEADER_AUTHORIZATION from gin.Context fail",
+			"JWT_HEADER_AUTHORIZATION", JWT_HEADER_AUTHORIZATION)
+		return ""
+	}
+	authHeaderStr, ok := authHeader.(string)
+	if !ok {
+		belogs.Error("GetAuthHeader(): assert to string fail", "authHeader", jsonutil.MarshalJson(authHeader))
+		return ""
+	}
+	return authHeaderStr
+}
---
+++ ginserver/ginjwt_test.go
@@ -0,0 +1,86 @@
+package ginserver
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cpusoft/goutil/conf"
+	"github.com/cpusoft/goutil/jwtutil"
+	_ "github.com/cpusoft/goutil/logs"
+	"github.com/cpusoft/goutil/zaplogs"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/sync/errgroup"
+)
+
+// start server
+func TestJwt(t *testing.T) {
+	zaplogs.DebugArgs(nil, "TestJwt(): start")
+	start := time.Now()
+	var g errgroup.Group
+
+	serverHttpPort := "1024"
+
+	gin.SetMode(gin.DebugMode)
+	engine := gin.New()
+	engine.Use(gin.Logger())
+	engine.Use(gin.Recovery())
+
+	public := engine.Group("/public")
+	{
+		public.POST("/hello", Hello)
+		public.POST("/login", Login)
+	}
+	auth := engine.Group("/auth")
+	{
+		RouterGroupRegisterJwt(auth)
+		auth.POST("/work", Work)
+	}
+	g.Go(func() error {
+		//	belogs.Info("startRpServer(): server run http on :", serverHttpPort)
+		zaplogs.InfoArgs(nil, "startRpServer(): server run http:", "port", serverHttpPort)
+		err := engine.Run(":" + serverHttpPort)
+		return err
+	})
+
+	if err := g.Wait(); err != nil {
+		//belogs.Error("startRpServer(): fail, will exit, err:", err)
+		zaplogs.ErrorArgs(nil, "startRpServer(): fail, will exit,", "port", serverHttpPort, " err:", err)
+	}
+	//belogs.Info("startRpServer(): server end, time(s):", time.Since(start))
+	zaplogs.InfoArgs(nil, "startRpServer(): server end", "time(s)", time.Since(start))
+
+}
+func Hello(c *gin.Context) {
+	String(c, "hello")
+}
+
+// will generate and return jwt token
+func Login(c *gin.Context) {
+	zaplogs.InfoArgs(nil, "Login(): start")
+	m := make(map[string]interface{})
+	m["ownerId"] = "1001"
+	m["ownerName"] = "beijing-user1"
+	m["opUserId"] = "2002"
+	m["opUserName"] = "beijing-user2"
+	m["traceId"] = "550e8400-e29b-41d4-a716-446655440000"
+	m["opLogId"] = "3003"
+
+	claims := jwtutil.CustomClaims{
+		Infos: m,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(2 * time.Hour)), //过期时间
+			NotBefore: jwt.NewNumericDate(time.Now()),                    //生效时间（立即生效）
+			IssuedAt:  jwt.NewNumericDate(time.Now()),                    //签发时间
+		},
+	}
+	token, _ := jwtutil.GenToken(&claims, conf.String("jwt::secret"))
+	zaplogs.InfoArgs(nil, "Login(): get claim:", "token", token)
+	String(c, token)
+}
+
+// will use jwt token in header, and will pass jwt check
+func Work(c *gin.Context) {
+	zaplogs.InfoArgs(SetToContextWithValue(c), "Work(): ", "start")
+	String(c, "work ok")
+}
