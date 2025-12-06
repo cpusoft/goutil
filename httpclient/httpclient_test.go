b85c06de4946b6efd027467d1ed06a4aa4b15fff
cpusoft <cpusoft@live.com>
2025-12-06 13:29:02 +0800

    postfiletest

--- httpclient/httpclient_test.go
+++ httpclient/httpclient_test.go
@@ -127,7 +127,7 @@ func TestPostFile(t *testing.T) {
 
 func TestHttpsPostFile(t *testing.T) {
 
-	resp, body, err := PostFileHttpWithConfig("http://10.1.135.22:8070/parsevalidate/parsefilesimple",
+	resp, body, err := PostFileHttpWithConfig("http://10.1.135.22:8070/debug-upload", //parsevalidate/parsefilesimple",
 		`/tmp/zzz.cer`, `file`, nil)
 	if err != nil {
 		fmt.Println(err)
