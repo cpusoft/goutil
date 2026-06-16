a4d69a9041bb99b19882a159e768d58a44021685
cpusoft <cpusoft@live.com>
2026-06-16 22:12:39 +0800

    badger

--- badgerutil/badgerutil.go
+++ badgerutil/badgerutil.go
@@ -33,7 +33,7 @@ func Init(dbPath string) error {
 	var opts badger.Options
 	if dbPath == "memory" {
 		opts = badger.DefaultOptions("").WithInMemory(true) // 开启内存模式
-		opts = opts.WithValueThreshold(10485765)            // 1MB
+		opts = opts.WithValueThreshold(1024*1024 + 1)       // 1MB
 
 	} else {
 		err = os.MkdirAll(dbPath, os.ModePerm)
